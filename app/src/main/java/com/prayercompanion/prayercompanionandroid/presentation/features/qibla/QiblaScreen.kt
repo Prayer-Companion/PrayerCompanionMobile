7deeb7820b433725c3198842bfa3e3803ac041f3
JosephJasin <Josef20606@gmail.com>
2023-05-24 20:26:29 +0300

    Beta 3

--- app/src/main/java/com/prayercompanion/prayercompanionandroid/presentation/features/qibla/QiblaScreen.kt
+++ app/src/main/java/com/prayercompanion/prayercompanionandroid/presentation/features/qibla/QiblaScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
@@ -26,7 +27,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.prayercompanion.prayercompanionandroid.R
@@ -37,7 +37,7 @@ import com.prayercompanion.prayercompanionandroid.domain.utils.SensorAccuracy
 fun QiblaScreen(
     viewModel: QiblaViewModel = hiltViewModel()
 ) {
-    val openDialog = remember { mutableStateOf(false) }
+    val isDialogOpen = remember { mutableStateOf(false) }
 
     DisposableEffect(key1 = viewModel) {
         viewModel.onStart()
@@ -46,9 +46,9 @@ fun QiblaScreen(
         }
     }
 
-    if (openDialog.value) {
+    if (isDialogOpen.value) {
         QiblaSensorAccuracyDialog {
-            openDialog.value = false
+            isDialogOpen.value = false
         }
     }
 
@@ -63,27 +63,29 @@ fun QiblaScreen(
         ) {
 
 
-            Spacer(modifier = Modifier.weight(0.5f))
+            Spacer(modifier = Modifier.weight(0.7f))
 
             Image(
                 modifier = Modifier
-                    .weight(1f)
+                    .size(280.dp)
                     .rotate(rotation),
                 painter = painterResource(R.drawable.qibla),
                 contentDescription = "",
             )
 
-            Spacer(modifier = Modifier.weight(0.5f))
+            Spacer(modifier = Modifier.weight(0.4f))
 
             Text(
                 modifier = Modifier.weight(1f),
-                text = "Degree = ${rotation}",
+                text = "${rotation.toInt()}°",
+                style = MaterialTheme.typography.subtitle1,
+                color = MaterialTheme.colors.onPrimary,
             )
 
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(start = 10.dp),
+                    .padding(start = 30.dp),
                 horizontalArrangement = Arrangement.Start,
                 verticalAlignment = Alignment.CenterVertically,
 
@@ -91,16 +93,16 @@ fun QiblaScreen(
                 QiblaSensorAccuracyIndicator(color = viewModel.sensorAccuracy.color)
 
                 Text(
-                    modifier = Modifier.padding(start = 5.dp),
+                    modifier = Modifier.padding(start = 10.dp),
                     text = stringResource(id = viewModel.sensorAccuracy.nameId),
-                    color = MaterialTheme.colors.secondary,
+                    style = MaterialTheme.typography.h3,
+                    color = MaterialTheme.colors.secondary
                 )
 
-//                if (viewModel.sensorAccuracy != SensorAccuracy.HIGH)
-                if (true) {
+                if (viewModel.sensorAccuracy != SensorAccuracy.HIGH) {
                     IconButton(
                         onClick = {
-                            openDialog.value = true
+                            isDialogOpen.value = true
                         },
                     ) {
                         Icon(
@@ -111,7 +113,7 @@ fun QiblaScreen(
                     }
                 }
             }
-            Spacer(modifier = Modifier.weight(0.2f))
+            Spacer(modifier = Modifier.weight(0.35f))
         }
     } ?: run {
         Text(text = "Retrieving location...")
--- app/src/main/java/com/prayercompanion/prayercompanionandroid/presentation/features/qibla/QiblaSensorAccuracyDialog.kt
+++ app/src/main/java/com/prayercompanion/prayercompanionandroid/presentation/features/qibla/QiblaSensorAccuracyDialog.kt
@@ -1,5 +1,6 @@
 package com.prayercompanion.prayercompanionandroid.presentation.features.qibla
 
+import android.os.Build.VERSION.SDK_INT
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -7,24 +8,36 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
+import coil.ImageLoader
+import coil.compose.rememberAsyncImagePainter
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
 import com.prayercompanion.prayercompanionandroid.R
-import com.prayercompanion.prayercompanionandroid.presentation.theme.White
 
 @Preview
 @Composable
 fun QiblaSensorAccuracyDialog(onDismissRequest: () -> Unit = {}) {
+    val imageLoader = ImageLoader.Builder(LocalContext.current)
+        .components {
+            if (SDK_INT >= 28) {
+                add(ImageDecoderDecoder.Factory())
+            } else {
+                add(GifDecoder.Factory())
+            }
+        }
+        .build()
+
     Dialog(
         onDismissRequest = onDismissRequest,
     ) {
@@ -33,7 +46,7 @@ fun QiblaSensorAccuracyDialog(onDismissRequest: () -> Unit = {}) {
         ) {
             Column(
                 modifier = Modifier
-                    .background(White)
+                    .background(MaterialTheme.colors.primary)
                     .padding(12.dp),
 
                 ) {
@@ -43,23 +56,29 @@ fun QiblaSensorAccuracyDialog(onDismissRequest: () -> Unit = {}) {
                     modifier = Modifier
                         .size(150.dp)
                         .align(alignment = Alignment.CenterHorizontally),
-                    painter = painterResource(id = R.drawable.eight_in_air),
+                    painter = rememberAsyncImagePainter(R.drawable.eight_in_air, imageLoader),
                     contentDescription = "8 Shape in air"
                 )
                 Text(
                     stringResource(id = R.string.qibla_dialog_sensor_accuracy_title),
-                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.h3,
+                    color = MaterialTheme.colors.onPrimary
                 )
                 Text(
                     stringResource(id = R.string.qibla_dialog_sensor_accuracy_text),
-                    fontSize = 12.sp
+                    style = MaterialTheme.typography.body2,
+                    color = MaterialTheme.colors.secondary
                 )
 
                 TextButton(
                     onClick = onDismissRequest,
                     modifier = Modifier.align(alignment = Alignment.End),
                 ) {
-                    Text(stringResource(id = R.string.ok))
+                    Text(
+                        stringResource(id = R.string.ok),
+                        color = MaterialTheme.colors.onSecondary,
+                        style = MaterialTheme.typography.h3,
+                    )
                 }
             }
         }
