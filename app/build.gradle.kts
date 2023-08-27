54bf28d67a7e9b79d65992a4fbc8b5f2fa39065d
Raed.O.Ghazal <raed.o.ghazal@gmail.com>
2023-08-27 10:36:35 +0300

    Release v1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.prayercompanion.prayercompanionandroid"
         minSdk = 26
         targetSdk = 33
-        versionCode = 5
-        versionName = "1.2.0"
+        versionCode = 6
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
