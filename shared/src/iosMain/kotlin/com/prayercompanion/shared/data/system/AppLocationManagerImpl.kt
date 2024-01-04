d1d3770728b73ffa243e6c14ccdd6f2bcd35bab6
Raed.O.Ghazal <raed.o.ghazal@gmail.com>
2024-01-04 16:44:11 +0300

    * add address fetching * clean up

--- shared/src/iosMain/kotlin/com/prayercompanion/shared/data/system/AppLocationManagerImpl.kt
+++ shared/src/iosMain/kotlin/com/prayercompanion/shared/data/system/AppLocationManagerImpl.kt
@@ -8,56 +8,81 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.withContext
+import platform.CoreLocation.CLGeocoder
 import platform.CoreLocation.CLHeading
 import platform.CoreLocation.CLLocation
 import platform.CoreLocation.CLLocationManager
 import platform.CoreLocation.CLLocationManagerDelegateProtocol
+import platform.CoreLocation.CLPlacemark
 import platform.darwin.NSObject
 
 class AppLocationManagerImpl : AppLocationManager {
 
-    private val l = MutableStateFlow<Location?>(null)
+    private val location = MutableStateFlow<Location?>(null)
+    private val address = MutableStateFlow<Address?>(null)
 
     init {
-        @OptIn(ExperimentalForeignApi::class)
-        object : CLLocationManagerDelegateProtocol, NSObject() {
-            private val locationManager = CLLocationManager()
+        LocationManager(
+            onLocationRetrieved = { location.value = it },
+            onAddressRetrieved = { address.value = it })
+    }
 
-            init {
-                locationManager.delegate = this
-                locationManager.startUpdatingLocation()
+    override suspend fun getLastKnownLocation(): Location? =
+        withContext(Dispatchers.Main) {
+            return@withContext location.first { it != null }
+        }
+
+    override suspend fun getAddressByLocation(location: Location?): Address? =
+        withContext(Dispatchers.Main) {
+            return@withContext address.first { it != null }
+        }
+
+    override fun getRequestLocationUpdates(onLocationRetrieved: (Location) -> Unit) = Unit
+}
+
+@OptIn(ExperimentalForeignApi::class)
+private class LocationManager(
+    private val onLocationRetrieved: (location: Location) -> Unit,
+    private val onAddressRetrieved: (address: Address) -> Unit
+) : CLLocationManagerDelegateProtocol, NSObject() {
+    private val locationManager = CLLocationManager()
+    private val clGeocoder = CLGeocoder()
+
+    init {
+        locationManager.delegate = this
+        locationManager.startUpdatingLocation()
+    }
+
+    override fun locationManager(manager: CLLocationManager, didUpdateLocations: List<*>) {
+        notify(didUpdateLocations.lastOrNull() as? CLLocation)
+    }
+
+    override fun locationManager(manager: CLLocationManager, didUpdateHeading: CLHeading) {
+        notify(manager.location)
+    }
+
+    fun notify(lastLocation: CLLocation?) {
+        lastLocation?.let { location ->
+            location.coordinate.useContents {
+                onLocationRetrieved(Location(latitude, longitude))
             }
 
-            override fun locationManager(manager: CLLocationManager, didUpdateLocations: List<*>) {
-                notify(didUpdateLocations.lastOrNull() as? CLLocation, manager.heading)
-            }
-
-            override fun locationManager(manager: CLLocationManager, didUpdateHeading: CLHeading) {
-                notify(manager.location, didUpdateHeading)
-            }
-
-            fun notify(lastLocation: CLLocation?, lastHeading: CLHeading?) {
-                lastLocation?.coordinate?.useContents {
-                    val loc = Location(latitude, longitude)
-                    l.value = loc
-                }
+            getAddressForLocation(location) {
+                onAddressRetrieved(it)
             }
         }
     }
 
-    override suspend fun getLastKnownLocation(): Location? = withContext(Dispatchers.Main){
-        return@withContext l.first()
-    }
+    private fun getAddressForLocation(location: CLLocation, callback: (Address) -> Unit)  {
+        return clGeocoder.reverseGeocodeLocation(location) { placeMarks, _ ->
+            val place = placeMarks?.firstOrNull() as CLPlacemark?
 
-    override suspend fun getAddressByLocation(location: Location?): Address? {
-        return Address(
-            countryCode = "JO", locality = "Amman"
-        )
-        TODO("ios")
-    }
+            val countryCode = place?.ISOcountryCode ?: return@reverseGeocodeLocation
+            val locality = place.administrativeArea ?: return@reverseGeocodeLocation
 
-    override fun getRequestLocationUpdates(onLocationRetrieved: (Location) -> Unit) {
-        return
-        TODO("ios")
+            val address = Address(countryCode, locality)
+            callback(address)
+        }
+
     }
 }
