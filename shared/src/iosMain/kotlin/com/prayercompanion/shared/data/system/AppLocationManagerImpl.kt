de5cd356230d55c24e92c8dda41e36b59d72a58f
Omar Al-Tal <63508919+omartall@users.noreply.github.com>
2024-01-04 16:45:33 +0300

    tech/ios retrieve location
    * retrieve location and address on iOS - to be cleaned
    
    Co-authored-by: Omar Altal <omar.altal@cafu.com>

--- shared/src/iosMain/kotlin/com/prayercompanion/shared/data/system/AppLocationManagerImpl.kt
+++ shared/src/iosMain/kotlin/com/prayercompanion/shared/data/system/AppLocationManagerImpl.kt
@@ -2,20 +2,87 @@ package com.prayercompanion.shared.data.system
 
 import com.prayercompanion.shared.domain.models.Location
 import com.prayercompanion.shared.domain.models.app.Address
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.useContents
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.withContext
+import platform.CoreLocation.CLGeocoder
+import platform.CoreLocation.CLHeading
+import platform.CoreLocation.CLLocation
+import platform.CoreLocation.CLLocationManager
+import platform.CoreLocation.CLLocationManagerDelegateProtocol
+import platform.CoreLocation.CLPlacemark
+import platform.darwin.NSObject
 
-class AppLocationManagerImpl: AppLocationManager {
-    override suspend fun getLastKnownLocation(): Location? {
-        return Location(latitude = 31.963158, longitude = 35.930359)
-        TODO("ios")
+class AppLocationManagerImpl : AppLocationManager {
+
+    private val location = MutableStateFlow<Location?>(null)
+    private val address = MutableStateFlow<Address?>(null)
+
+    init {
+        LocationManager(
+            onLocationRetrieved = { location.value = it },
+            onAddressRetrieved = { address.value = it })
     }
-    override suspend fun getAddressByLocation(location: Location?): Address? {
-        return Address(
-            countryCode = "JO", locality = "Amman"
-        )
-        TODO("ios")
+
+    override suspend fun getLastKnownLocation(): Location? =
+        withContext(Dispatchers.Main) {
+            return@withContext location.first { it != null }
+        }
+
+    override suspend fun getAddressByLocation(location: Location?): Address? =
+        withContext(Dispatchers.Main) {
+            return@withContext address.first { it != null }
+        }
+
+    override fun getRequestLocationUpdates(onLocationRetrieved: (Location) -> Unit) = Unit
+}
+
+@OptIn(ExperimentalForeignApi::class)
+private class LocationManager(
+    private val onLocationRetrieved: (location: Location) -> Unit,
+    private val onAddressRetrieved: (address: Address) -> Unit
+) : CLLocationManagerDelegateProtocol, NSObject() {
+    private val locationManager = CLLocationManager()
+    private val clGeocoder = CLGeocoder()
+
+    init {
+        locationManager.delegate = this
+        locationManager.startUpdatingLocation()
     }
-    override fun getRequestLocationUpdates(onLocationRetrieved: (Location) -> Unit) {
-        return
-        TODO("ios")
+
+    override fun locationManager(manager: CLLocationManager, didUpdateLocations: List<*>) {
+        notify(didUpdateLocations.lastOrNull() as? CLLocation)
+    }
+
+    override fun locationManager(manager: CLLocationManager, didUpdateHeading: CLHeading) {
+        notify(manager.location)
+    }
+
+    fun notify(lastLocation: CLLocation?) {
+        lastLocation?.let { location ->
+            location.coordinate.useContents {
+                onLocationRetrieved(Location(latitude, longitude))
+            }
+
+            getAddressForLocation(location) {
+                onAddressRetrieved(it)
+            }
+        }
+    }
+
+    private fun getAddressForLocation(location: CLLocation, callback: (Address) -> Unit)  {
+        return clGeocoder.reverseGeocodeLocation(location) { placeMarks, _ ->
+            val place = placeMarks?.firstOrNull() as CLPlacemark?
+
+            val countryCode = place?.ISOcountryCode ?: return@reverseGeocodeLocation
+            val locality = place.administrativeArea ?: return@reverseGeocodeLocation
+
+            val address = Address(countryCode, locality)
+            callback(address)
+        }
+
     }
 }
