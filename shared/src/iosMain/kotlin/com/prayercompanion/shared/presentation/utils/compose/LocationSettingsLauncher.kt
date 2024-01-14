1ee734ca5926cb9e465c3c2fcb9d7678267f3692
Raed.O.Ghazal <raed.o.ghazal@gmail.com>
2024-01-14 12:23:48 +0300

    * Implement LocationSettingsLauncher in iOS

--- shared/src/iosMain/kotlin/com/prayercompanion/shared/presentation/utils/compose/LocationSettingsLauncher.kt
+++ shared/src/iosMain/kotlin/com/prayercompanion/shared/presentation/utils/compose/LocationSettingsLauncher.kt
@@ -1,9 +1,34 @@
 package com.prayercompanion.shared.presentation.utils.compose
 
 import androidx.compose.runtime.Composable
+import platform.Foundation.NSURL
+import platform.UIKit.UIApplication
+import platform.UIKit.UIApplicationOpenSettingsURLString
 
 @Composable
 actual fun LocationSettingsLauncher(onResult: (isResultOk: Boolean) -> Unit): () -> Unit {
-    return {}
-    TODO("ios")
+    return Callback@{
+        val url = NSURL.URLWithString(UIApplicationOpenSettingsURLString) ?: return@Callback
+        val canOpen = UIApplication.sharedApplication.canOpenURL(url)
+
+        if (canOpen) {
+            UIApplication.sharedApplication.openURL(url)
+        }
+    }
 }
+
+/*
+if let url = URL(string:UIApplication.openSettingsURLString)
+
+
+
+{ if UIApplication.shared.canOpenURL(url)
+
+{ UIApplication.shared.open(url, options: [:], completionHandler: nil)
+
+
+
+}
+
+}
+* */
